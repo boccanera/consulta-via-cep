41d6d8e076ce81c4145cb12573ccb7c072b4c51c
boccanera <101893308+boccanera@users.noreply.github.com>
2022-07-25 11:59:44 -0300

    Delete webservice-calculo-de-frete directory

--- webservice-calculo-de-frete/src/main/java/exception/ViaCepException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ViaCepException extends RuntimeException {
-    public ViaCepException(String s) {
-        super(s);
-    }
-}
--- webservice-calculo-de-frete/src/main/java/exception/ViaCepFormatException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ViaCepFormatException extends RuntimeException {
-    public ViaCepFormatException(String s) {
-        super(s);
-    }
-}
