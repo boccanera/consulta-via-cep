a29464215e9a9ca28674111a3bbb6d2069f47369
boccanera <101893308+boccanera@users.noreply.github.com>
2022-07-25 12:00:25 -0300

    Delete webservice-consulta-frete-api directory

--- webservice-consulta-frete-api/src/main/java/com/example/demo/exception/ViaCepException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ViaCepException extends RuntimeException {
-    public ViaCepException(String s) {
-        super(s);
-    }
-}
--- webservice-consulta-frete-api/src/main/java/com/example/demo/exception/ViaCepFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ViaCepFormatException extends RuntimeException {
-    public ViaCepFormatException(String s) {
-        super(s);
-    }
-}
