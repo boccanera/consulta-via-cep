d94c89898edd28a382a9bf4d23ccf939a505016f
boccanera <101893308+boccanera@users.noreply.github.com>
2022-07-25 12:00:16 -0300

    Delete webservice-consulta-de-cep directory

--- webservice-consulta-de-cep/src/main/java/com/project/exception/ViaCepException.java
+++
@@ -1,7 +0,0 @@
-package com.project.exception;
-
-public class ViaCepException extends RuntimeException {
-    public ViaCepException(String s) {
-        super(s);
-    }
-}
--- webservice-consulta-de-cep/src/main/java/com/project/exception/ViaCepFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.project.exception;
-
-public class ViaCepFormatException extends RuntimeException {
-    public ViaCepFormatException(String s) {
-        super(s);
-    }
-}
