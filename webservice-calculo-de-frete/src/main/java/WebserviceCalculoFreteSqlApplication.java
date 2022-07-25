89c32dc4339b00a0da0837c389caf187c6a0584f
boccanera <nailtonboccanera@gmail.com>
2022-07-25 11:57:08 -0300

    consulta de webservice viacep

---
+++ webservice-calculo-de-frete/src/main/java/WebserviceCalculoFreteSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculoFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceCalculoFreteSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceCalculoFreteSqlApplication.class, args);
+	}
+
+}
---
+++ webservice-calculo-de-frete/src/test/java/com/example/calculoFrete/WebserviceCalculoFreteSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculoFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceCalculoFreteSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
